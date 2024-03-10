e098605ce9bdef4110453ab1b04da67087d14f2f
Taiba053 <157193726+Taiba053@users.noreply.github.com>
2024-03-10 22:27:56 +0530

    Update stud.java

--- stud.java
+++ stud.java
@@ -1,30 +1,31 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
-class Student {
+class stud {
     String name;
     int rollno;
     int math, phy, eng;
 
+    public static void main(String[] args) {
+        stud s = new stud();
+        s.getStudentDetails();
+        s.displayStudentDetails();
+    }
+
     void getStudentDetails() {
-        // removed user input
-        name = "John Doe";
-        rollno = 12345;
-        math = 85;
-        phy = 90;
-        eng = 80;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        name = sc.next();
+        System.out.println("Enter Roll Number: ");
+        rollno = sc.nextInt();
+        System.out.println("Enter marks in Maths, Physics, and English: ");
+        math = sc.nextInt();
+        phy = sc.nextInt();
+        eng = sc.nextInt();
     }
 
     void displayStudentDetails() {
         System.out.println("Roll Number: " + rollno + "\nName: " + name);
         System.out.println("Marks (Maths, Physics, English): " + math + "," + phy + "," + eng);
         System.out.println("Total: " + (math + phy + eng) + "\tPercentage: " + ((math + phy + eng) * 100 / 300));
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-        Student s = new Student();
-        s.getStudentDetails();
-        s.displayStudentDetails();
     }
 }
