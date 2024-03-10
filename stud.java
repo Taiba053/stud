a431834c0ccc96ac198533dec687d21e82c95f8b
Taiba <01fe22bca053@kletech.ac.in>
2024-03-10 20:57:51 +0530

    stud details

---
+++ stud.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class stud {
+    String name;
+    int rollno;
+    int math, phy, eng;
+
+    void getStudentDetails() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        name = sc.next();
+        System.out.println("Enter Roll Number: ");
+        rollno = sc.nextInt();
+        System.out.println("Enter marks in Maths, Physics, and English: ");
+        math = sc.nextInt();
+        phy = sc.nextInt();
+        eng = sc.nextInt();
+    }
+
+    void displayStudentDetails() {
+        System.out.println("Roll Number: " + rollno + "\nName: " + name);
+        System.out.println("Marks (Maths, Physics, English): " + math + "," + phy + "," + eng);
+        System.out.println("Total: " + (math + phy + eng) + "\tPercentage: " + ((math + phy + eng) * 100 / 300));
+    }
+}
+
+ class main {
+    public static void main(String[] args) {
+        stud s = new stud();
+        s.getStudentDetails();
+        s.displayStudentDetails();
+    }
+}
+
